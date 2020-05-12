a36d83c7d201aa93fc3872753170a8b064cc95fb
yaoyaoustc <yaoyaoustc@gmail.com>
2020-05-12 12:39:05 +1000

    add navbar

---
+++ react-test/confusion/src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+    return(
+        <div className="container">
+            <h4>This is Home page</h4>
+        </div>
+    )
+}
+
+export default Home;
--- react-test/confusion/src/components/MainComponent.js
+++ react-test/confusion/src/components/MainComponent.js
@@ -4,29 +4,44 @@ import Webs from './WebsComponent-func';
 import WebDetails from './WebDetailsComponent-func';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 // import WebDetails from './WebDetailsComponent';
 import { WEBS } from '../shared/webs';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       webs: WEBS,
-      selectedWeb: null
+      // selectedWeb: null
     };
   }
 
-  onWebSelect(webId) {
-    this.setState({ selectedWeb: webId});
-  }
+  // onWebSelect(webId) {
+  //   this.setState({ selectedWeb: webId});
+  // }
 
   render() {
+
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
+
     return (
-      <div className="App">
+      <div>
         <Header />
-        <Webs webs = {this.state.webs} onClick={(webId) => this.onWebSelect(webId)}/>
-        <WebDetails 
-            selectedWeb = {this.state.webs.filter((web) => web.id === this.state.selectedWeb)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path = "/webs" component = {() => <Webs webs = {this.state.webs} />} />
+          <Redirect to = "/home" />
+        
+        </Switch> 
+        {/* <Webs webs = {this.state.webs} onClick={(webId) => this.onWebSelect(webId)}/> */}
+        {/* <WebDetails  */}
+            {/* selectedWeb = {this.state.webs.filter((web) => web.id === this.state.selectedWeb)[0]} /> */}
         <Footer />
       </div>
     )
